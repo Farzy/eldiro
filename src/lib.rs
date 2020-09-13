a22d4b56c4d1ebf4b4450cc055ee14897e4330ca
Farzad FARID <farzy@farzy.org>
2020-09-14 00:03:06 +0200

    Parse operators

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,51 @@ impl Number {
     }
 }
 
+#[derive(Debug, PartialEq)]
+pub enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+impl Op {
+    pub fn new(s: &str) -> Self {
+        match s {
+            "+" => Self::Add,
+            "-" => Self::Sub,
+            "*" => Self::Mul,
+            "/" => Self::Div,
+            _ => panic!("bar operator"),
+        }
+    }
+}
+
 mod tests {
     use super::*;
 
     #[test]
     fn parse_number() {
         assert_eq!(Number::new("123"), Number(123));
+    }
+
+    #[test]
+    fn parse_add_op() {
+        assert_eq!(Op::new("+"), Op::Add);
+    }
+
+    #[test]
+    fn parse_sub_op() {
+        assert_eq!(Op::new("-"), Op::Sub);
+    }
+
+    #[test]
+    fn parse_mul_op() {
+        assert_eq!(Op::new("*"), Op::Mul);
+    }
+
+    #[test]
+    fn parse_div_op() {
+        assert_eq!(Op::new("/"), Op::Div);
     }
 }
