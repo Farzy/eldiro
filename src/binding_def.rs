098331a7b7d73d6e9dfeb424d96b665171942c0d
Farzad FARID <farzad.farid@kapten.com>
2020-09-14 16:03:21 +0200

    Implement BindingDef::new

--- src/binding_def.rs
+++ src/binding_def.rs
@@ -1,14 +1,40 @@
 use crate::expr::Expr;
+use crate::util::extract_whitespace;
 
 #[derive(Debug, PartialEq)]
 pub struct BindingDef {
     name: String,
-    var: Expr,
+    val: Expr,
 }
 
 impl BindingDef {
     pub fn new(s: &str) -> (&str, Self) {
-        todo!()
+        let s = if s.starts_with("let") {
+            &s[3..]
+        } else {
+            panic!("expected let")
+        };
+        let (s, _) = extract_whitespace(s);
+
+        let (s, name) = extract_ident(s);
+        let (s, _) = extract_whitespace(s);
+
+        let s = if s.starts_with('=') {
+            &s[1..]
+        } else {
+            panic!("expected =")
+        };
+        let (s, _) = extract_whitespace(s);
+
+        let (s, val) = Expr::new(s);
+
+        (
+            s,
+            Self {
+                name: name.to_string(),
+                val,
+            },
+        )
     }
 }
 
@@ -25,7 +51,7 @@ mod test {
                 "",
                 BindingDef {
                     name: "a".to_string(),
-                    var: Expr {
+                    val: Expr {
                         lhs: Number(10),
                         rhs: Number(2),
                         op: Op::Div,
